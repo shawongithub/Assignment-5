4601987b1f425a13b2624df75038f068adc5c7c9
shawon <shawonislam121009@gmail.com>
2021-02-07 23:47:06 +0600

    second commit

--- app.js
+++ app.js
@@ -11,8 +11,7 @@ document.getElementById('search-btn').addEventListener('click', function () {
 
 
 const displayMeals = meals => {
-    console.log(meals)
-
+    document.getElementById('input').value = ''
     const container = document.getElementById("all-div")
     meals.forEach(meal => {
         const mealDiv = document.createElement('div')
@@ -29,19 +28,38 @@ const displayMeals = meals => {
         container.appendChild(mealDiv)
     });
 }
+const displayError = () => {
+    const container = document.getElementById("all-div")
+    const mealDiv = document.createElement('div')
+    mealDiv.className = "meal-class"
+    const mealInfo = `
+    <h1>No result found</h1>
+    `
+    mealDiv.innerHTML = mealInfo
+    container.appendChild(mealDiv)
+}
 const detailFetch = id => {
     const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`
     fetch(url)
         .then(res => res.json())
-        .then(data => console.log(data.meals[0]))
+        .then(data => displayDetail(data.meals[0]))
 }
 
-// const displayDetail = meal => {
-//     const detailContainer = document.getElementById("detail-div")
-//     detailContainer.className = "detail"
-//     const detailInfo = `
-//         <img src="${}">
-//         <h3>${country.name}</h3>
-//     `
-//     detailContainer.innerHTML = detailInfo
-// }
+const displayDetail = meal => {
+    console.log(meal)
+    const detailContainer = document.getElementById("detail-div")
+    detailContainer.className = "detail"
+    const detailInfo = `
+        <img src="${meal.strMealThumb}">
+        <br>
+        <h5>${meal.strMeal}</h5>
+        <ul>
+            <li>${meal.strIngredient1}</li>
+            <li>${meal.strIngredient2}</li>
+            <li>${meal.strIngredient3}</li>
+            <li>${meal.strIngredient4}</li>
+            <li>${meal.strIngredient5}</li>
+        </ul>
+    `
+    detailContainer.innerHTML = detailInfo
+}
